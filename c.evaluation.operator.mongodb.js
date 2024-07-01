452a005f566b99b14d378c9d0b8a3dd1e6cf28f4
neupanesaugat <saugatneupane50@gmail.com>
2024-07-01 18:19:24 +0545

    updated evaluation operators in mongoDB

--- c.evaluation.operator.mongodb.js
+++ c.evaluation.operator.mongodb.js
@@ -17,7 +17,11 @@ use("kec-crud");
 //? if there is no value to compare and you have to compare between two fields
 
 //? find employees whose expense is greater than their income
-db.employee.find({ $expr: { $gt: ["$expense", "$income"] } }); //? $expense and $income define it's field
+// db.employee.find({ $expr: { $gt: ["$expense", "$income"] } }); //? $expense and $income define it's field
 
 //? find employee whose income is less than 850
 // db.employee.find({ income: { $lt: 850 } });
+
+//?alternative: (income<850)
+
+// db.employee.find({ $expr: { $gt: ["$income", 850] } });
